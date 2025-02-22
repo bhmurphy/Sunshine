ac48a0ba71b09c581231869131579a8f3f2d4b08
Hunter Murphy <bhm5zr@virginia.edu>
2025-02-22 11:04:38 -0500

    add a dumb log

--- src/httpcommon.cpp
+++ src/httpcommon.cpp
@@ -201,6 +201,9 @@ namespace http {
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fwrite);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+    std::string profile = getenv("USERPROFILE");
+    curl_easy_setopt(curl, CURLOPT_STDERR, fopen((profile + "test.log").c_str()));
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
 
     CURLcode result = curl_easy_perform(curl);
     if (result != CURLE_OK) {
